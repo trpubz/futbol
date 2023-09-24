a11948e7b4af2e4c30019a623ddaf9792371cbcb
Taylor Pubins <tpubz@icloud.com>
2023-09-24 15:36:10 -0700

    Add filters for spec/ and ./data/ directories, as well as ./lib/runner.rb file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,9 @@
 require "simplecov"
 SimpleCov.start do
   enable_coverage :branch
-  add_filter "/spec/"
-  add_filter "/data/"
+  add_filter "spec/"
+  add_filter "./data/"
+  add_filter "./lib/runner.rb"
 end
 
 require "csv"
